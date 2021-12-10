7a5f85493b7698cd3d8c8857fd6af959e4a1bfe3
bhavanakonchada <bhavanakonchada@users.noreply.github.com>
2021-12-09 20:17:32 -0800

    Create joke.js

---
+++ .github/actions/joke-action/joke.js
@@ -0,0 +1,18 @@
+const request = require("request-promise")
+
+const options = {
+  method: "GET",
+  uri: "https://icanhazdadjoke.com/",
+  headers:{
+    Accept: "application/json",
+    "User-Agent": "Bhavana writing Actions"
+  },
+  json: true
+};
+
+async function getJoke(){
+  const result = await result(options);
+  return result.joke;
+}
+
+module.export = getJoke;
